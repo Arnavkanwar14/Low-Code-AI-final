d98ff5ea7f3c49ea4be1a2055f31550afe61bb04
Arnavkanwar14 <arnavkanwar1404@gmail.com>
2026-06-23 22:35:30 +0530

    Update text.py

--- backend/preprocessing/text.py
+++ backend/preprocessing/text.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sentence_transformers import SentenceTransformer
 
 class TextEmbedder:
     def __init__(self, model_name="all-MiniLM-L6-v2"):
+        from sentence_transformers import SentenceTransformer
         self.model = SentenceTransformer(model_name)
 
     def fit(self, X):
